8ca9920bf44a7e4a3d45b6a159cef1738cf37163
David Aronchick <aronchick@gmail.com>
2022-10-13 21:35:41 -0700

    updating Make v1

--- stablediffusiongallery/tf/modules/instance/output.tf
+++
@@ -1,3 +0,0 @@
-output "instance_eip" {
-  value = "${aws_eip.testInstanceEip.public_ip}"
-}
--- stablediffusiongallery/tf/modules/securityGroup/output.tf
+++
@@ -1,7 +0,0 @@
-output "sg_22" {
-  value = "${aws_security_group.sg_22.id}"
-}
-
-output "sg_80" {
-  value = "${aws_security_group.sg_80.id}"
-}
