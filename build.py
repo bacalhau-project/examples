574a48996acfda8dd73709f00a66618e2d73325a
David Aronchick <aronchick@gmail.com>
2022-11-15 12:59:36 -0800

    Stablediffusiongallery (#16)
    * final done

---
+++ build.py
@@ -0,0 +1,53 @@
+import glob
+import os
+from pathlib import Path
+
+import nbconvert
+import shutil
+
+src_dir = Path(".")
+dst_dir = Path("rendered")
+
+all_notebooks = set(glob.glob("**/*.ipynb", recursive=True))
+
+notebook_exclude_directories = ("templates", "todo")
+notebooks_to_render = list(filter(lambda x: not x.startswith(notebook_exclude_directories), all_notebooks))
+
+image_suffixes = (".png", ".jpg", ".jpeg", ".gif", ".svg")
+image_exclude_directories = ("templates", "todo", "rendered")
+image_files_to_copy = []
+for image_suffix in image_suffixes:
+    image_files = glob.glob(f"**/*{image_suffix}", recursive=True)
+    for i in image_files:
+        if not i.startswith(image_exclude_directories):
+            image_files_to_copy.append(i)
+
+for f in notebooks_to_render:
+    markdown_string = nbconvert.markdown.MarkdownExporter(
+        config={
+            "TagRemovePreprocessor": {
+                "enabled": True,
+                "remove_cell_tags": ("remove_cell",),
+                "remove_all_outputs_tags": ("remove_output",),
+                "remove_input_tags": ("remove_input",),
+            },
+            "ImageExtractor": {
+                "enabled": True,
+                "output_directory": f"{dst_dir}/{os.path.dirname(f)}",
+                "extract_output_types": ("image/png", "image/jpeg", "image/gif", "image/svg+xml"),
+            },
+        },
+    ).from_filename(
+        f,
+        output_file=Path(dst_dir) / Path(os.path.splitext(f)[0] + ".md"),
+    )
+    stripped_markdown_string = markdown_string[0].replace("%%bash", "")
+
+    notebook_destination_dir = Path(dst_dir) / os.path.dirname(f)
+    Path.exists(notebook_destination_dir) or Path(notebook_destination_dir).mkdir(parents=True)
+    Path.write_text(Path(os.path.join(dst_dir, os.path.splitext(f)[0] + ".md")), stripped_markdown_string)
+
+for i in image_files_to_copy:
+    image_destination_dir = Path(dst_dir) / os.path.dirname(i)
+    Path.exists(image_destination_dir) or Path(image_destination_dir).mkdir(parents=True)
+    shutil.copyfile(i, image_destination_dir / os.path.basename(i))
---
+++ stablediffusiongallery/tf/variable.tf
@@ -0,0 +1,8 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "availability_zone" {}
+variable "app_tag" {}
+variable "public_key_path" {}
+variable "instance_type" {}
+variable "instance_ami" {}
