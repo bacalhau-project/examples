d7db31520574dfff28caf7567a16a1c02703aea3
Phil Winder <phil@Winder.AI>
2022-12-08 12:18:43 +0000

    refactor(upload): switch to zerolog to output to stdout
    Former-commit-id: 03cac7d8b61533f871dcaf0bd3577426a6b4cb27

--- tools/upload/config/global.go
+++ tools/upload/config/global.go
@@ -1,10 +1,9 @@
 package config
 
 import (
-	"os"
+	"fmt"
 	"path/filepath"
 
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -21,21 +20,20 @@ type GlobalConfig struct {
 	OutputPath string `yaml:"output-path"`
 }
 
-func ParseGlobalConfig(cmd *cobra.Command) *GlobalConfig {
+func ParseGlobalConfig(cmd *cobra.Command) (*GlobalConfig, error) {
 	number, err := cmd.Flags().GetInt(LogLevelFlag)
 	if err != nil {
-		logrus.Errorf("error parsing log level: %s", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("parsing log level: %w", err)
 	}
 	return &GlobalConfig{
 		LogLevel:   number,
 		InputPath:  filepath.Clean(cmd.Flag(InputPathFlag).Value.String()),
 		OutputPath: filepath.Clean(cmd.Flag(OutputPathFlag).Value.String()),
-	}
+	}, nil
 }
 
 func AddGlobalFlags(cmd *cobra.Command) *cobra.Command {
-	cmd.PersistentFlags().IntP(LogLevelFlag, "l", 4, "Logging level (5=debug, 4=info, 3=warning, 2=error")
+	cmd.PersistentFlags().IntP(LogLevelFlag, "l", 1, "Logging level (-1= trace, 0=debug, 1=info, 2=warning, 3=error, ...")
 	cmd.PersistentFlags().StringP(InputPathFlag, "i", "/inputs", "Input path")
 	cmd.PersistentFlags().StringP(OutputPathFlag, "o", "/outputs", "Output path")
 	return cmd
