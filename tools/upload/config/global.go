63fd5a72255349b26d5f6c5f8f6aeeb693637aa6
Phil Winder <phil@Winder.AI>
2022-12-08 12:33:02 +0000

    refactor(upload): add logging formats

--- tools/upload/config/global.go
+++ tools/upload/config/global.go
@@ -10,12 +10,14 @@ import (
 // Define all global flag names
 const (
 	LogLevelFlag   = "log-level"
+	LogFormatFlag  = "log-format"
 	InputPathFlag  = "input"
 	OutputPathFlag = "output"
 )
 
 type GlobalConfig struct {
 	LogLevel   int    `yaml:"log-level"`
+	LogFormat  string `yaml:"log-format"`
 	InputPath  string `yaml:"input-path"`
 	OutputPath string `yaml:"output-path"`
 }
@@ -27,6 +29,7 @@ func ParseGlobalConfig(cmd *cobra.Command) (*GlobalConfig, error) {
 	}
 	return &GlobalConfig{
 		LogLevel:   number,
+		LogFormat:  cmd.Flag(LogFormatFlag).Value.String(),
 		InputPath:  filepath.Clean(cmd.Flag(InputPathFlag).Value.String()),
 		OutputPath: filepath.Clean(cmd.Flag(OutputPathFlag).Value.String()),
 	}, nil
@@ -34,6 +37,7 @@ func ParseGlobalConfig(cmd *cobra.Command) (*GlobalConfig, error) {
 
 func AddGlobalFlags(cmd *cobra.Command) *cobra.Command {
 	cmd.PersistentFlags().IntP(LogLevelFlag, "l", 1, "Logging level (-1= trace, 0=debug, 1=info, 2=warning, 3=error, ...")
+	cmd.PersistentFlags().StringP(LogFormatFlag, "", "text", "Logging format (json, text)")
 	cmd.PersistentFlags().StringP(InputPathFlag, "i", "/inputs", "Input path")
 	cmd.PersistentFlags().StringP(OutputPathFlag, "o", "/outputs", "Output path")
 	return cmd
