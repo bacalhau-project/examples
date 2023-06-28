671249451902f8f39dbc0cdad5737595466d5184
Vedant Padwal <79689323+js-ts@users.noreply.github.com>
2023-06-28 16:38:31 +0000

    copy instead of download

--- multi-region/populate_buckets.py
+++ multi-region/populate_buckets.py
@@ -5,55 +5,41 @@ import random
 import boto3
 import terraform
 
-def populate_s3_buckets(bucket_names, image_dir, sample_size):
-    s3 = boto3.client("s3")
+def copy_random_images_to_s3_buckets(src_bucket_name, dst_bucket_names, s3, sample_size):
+    all_objects = s3.list_objects(Bucket=src_bucket_name)['Contents']
+    all_images = [obj['Key'] for obj in all_objects if obj['Key'].endswith(('.png', '.jpg', '.jpeg'))]
+    sample_images = random.sample(all_images, sample_size)
 
-    for bucket_name in bucket_names:
-        files = os.listdir(image_dir)
-        sample_files = random.sample(files, sample_size)
+    for dst_bucket_name in dst_bucket_names:
+        for image_name in sample_images:
+            copy_source = {
+                'Bucket': src_bucket_name,
+                'Key': image_name
+            }
+            s3.copy(copy_source, dst_bucket_name, image_name)
+            print(f"Copied {image_name} from {src_bucket_name} to {dst_bucket_name}")
 
-        for file_name in sample_files:
-            file_path = os.path.join(image_dir, file_name)
-            s3.upload_file(file_path, bucket_name, file_name)
-            print(f"Uploaded {file_name} to {bucket_name}")
-
-def retrieve_s3_bucket_info(working_dir, sample_size):
-    # Initialize the Terraform API
+def retrieve_s3_bucket_info(src_bucket_name, working_dir, sample_size):
+    s3 = boto3.client("s3", region_name="eu-north-1")
     tf = terraform.Terraform(working_dir)
-
-    # Run 'terraform init' to initialize the working directory
     tf.init()
-
-    # Run 'terraform refresh' to update the state file
     tf.refresh()
 
-    # Get the Terraform state
     state = tf.state()
-
-    # Retrieve information about S3 buckets
     s3_buckets = state.get_resources_by_type("aws_s3_bucket")
+    dst_bucket_names = [bucket["name"] for bucket in s3_buckets]
 
-    # Extract the bucket names
-    bucket_names = [bucket["name"] for bucket in s3_buckets]
-
-    # Populate S3 buckets with images
-    image_dir = "/path/to/coco/validation/images"  # Path to the COCO validation image directory
-    populate_s3_buckets(bucket_names, image_dir, sample_size)
+    copy_random_images_to_s3_buckets(src_bucket_name, dst_bucket_names, s3, sample_size)
 
 if __name__ == "__main__":
-    # Create an argument parser
-    parser = argparse.ArgumentParser(description="Retrieve S3 bucket information using Terraform and populate with COCO dataset images.")
+    parser = argparse.ArgumentParser(description="Retrieve S3 bucket information using Terraform and copy a random set of images from a source S3 bucket.")
 
-    # Add the working_dir argument with a default value of the current working directory
-    parser.add_argument("working_dir", type=str, nargs="?", default=os.getcwd(),
+    parser.add_argument("working_dir", type=str, nargs="?", default=os.getcwd(), 
                         help="Path to the Terraform working directory. Default: current working directory")
+    parser.add_argument("--sample_size", type=int, default=10, 
+                        help="Number of images to sample from the source S3 bucket. Default: 10")
 
-    # Add the sample_size argument with a default value of 10
-    parser.add_argument("--sample_size", type=int, default=10,
-                        help="Number of images to sample from the COCO dataset. Default: 10")
-
-    # Parse the command-line arguments
     args = parser.parse_args()
-
-    # Call the function with the provided working directory and sample size
-    retrieve_s3_bucket_info(args.working_dir, args.sample_size)
+    src_bucket_name = "sam-coco"
+    
+    retrieve_s3_bucket_info(src_bucket_name, args.working_dir, args.sample_size)
