ce50d12321410e0c944545fe6ae43119e1de4535
Vedant Padwal <79689323+js-ts@users.noreply.github.com>
2023-07-05 14:06:35 +0000

    Add Prompt

--- multi-region/job.py
+++ multi-region/job.py
@@ -1,11 +1,21 @@
 import json
 import concurrent.futures
 import subprocess
+import argparse
 
 def run_command(cmd):
     process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     return stdout, stderr, process.returncode
+
+# Create the parser
+parser = argparse.ArgumentParser(description="Execute docker commands")
+
+# Add the arguments
+parser.add_argument('--prompt_value', type=str, help="The prompt value for the Docker command", default='person')
+
+# Parse the arguments
+args = parser.parse_args()
 
 # Load the .env.json file
 with open('./tf/.env.json') as json_file:
@@ -26,8 +36,8 @@ for region in data["locations"].keys():
     # Format the Docker run command
     command = (f'bacalhau docker run --gpu 1 -i src=s3://{input_bucket}/*,opt=region={region} '
                f'-p s3://{output_bucket}/*,opt=region={region} -s region={region} '
-               'expanso/sam:new -- /bin/bash -c \'python /sam.py --input "/inputs" '
-               '--output "/outputs" --prompt "person"\'')
+               f'expanso/sam:new -- /bin/bash -c \'python /sam.py --input "/inputs" '
+               f'--output "/outputs" --prompt "{args.prompt_value}"\'')
     
     # Add command to the list
     commands.append(command)
