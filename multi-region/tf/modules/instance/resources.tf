422bc8be843fc0ffad1badc3efc1978eeecbdc99
Vedant Padwal <79689323+js-ts@users.noreply.github.com>
2023-08-04 10:13:40 +0000

    remove input bucket

--- multi-region/tf/modules/instance/resources.tf
+++ multi-region/tf/modules/instance/resources.tf
@@ -71,15 +71,6 @@ resource "aws_iam_role_policy" "vm_iam_role_policy" {
 EOF
 }
 
-resource "aws_s3_bucket" "images_bucket" {
-  bucket = "${var.app_tag}-${var.region}-images-bucket"
-  # Force delete even if not empty
-  force_destroy = true
-  tags = {
-    Name = var.app_tag
-  }
-}
-
 resource "aws_s3_bucket" "output_images_bucket" {
   bucket = "${var.app_tag}-${var.region}-o-images-bucket"
   # Force delete even if not empty
