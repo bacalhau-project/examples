c4f3a37bc4c4c9cf0f739ed894be9ece3e7a0bd3
js-ts <vedantpadwalinfi@gmail.com>
2023-09-25 13:26:10 +0530

    Use Absolute path

--- multi-cloud/bulk-deploy.sh
+++ multi-cloud/bulk-deploy.sh
@@ -2,7 +2,8 @@
 
 # AWS Operations
 aws_directory="tf/aws"
-cd $aws_directory || exit
+aws_directory_path=$(realpath "$aws_directory")
+cd $aws_directory_path || exit
 
 # Load a list of zones from regions.md, as long as they are not commented out or blank
 aws_regions=()
@@ -12,14 +13,14 @@ while IFS= read -r line; do
     fi
 done < ../regions.md
 
-if [[ "$1" == "create" && "$PWD" == "$aws_directory" ]]; then
+if [[ "$1" == "create" && "$PWD" == "$aws_directory_path" ]]; then
     for r in "${aws_regions[@]}"
     do
         terraform workspace select -or-create "$r"
         terraform init -upgrade
         terraform apply -auto-approve -var "region=$r" -var-file=.env.json
     done
-elif [[ "$1" == "destroy" && "$PWD" == "$aws_directory" ]]; then
+elif [[ "$1" == "destroy" && "$PWD" == "$aws_directory_path" ]]; then
     for r in "${aws_regions[@]}"
     do
         terraform workspace select -or-create "$r"
@@ -32,16 +33,17 @@ fi
 
 # GCP Operations
 gcp_directory="../gcp"
-cd $gcp_directory || exit
+gcp_directory_path=$(realpath "$gcp_directory")
+cd $gcp_directory_path || exit
 
 # Assume the regions for GCP are also in regions.md
 
-if [[ "$1" == "create" && "$PWD" == "$gcp_directory" ]]; then
+if [[ "$1" == "create" && "$PWD" == "$gcp_directory_path" ]]; then
     terraform init
     terraform plan -out plan.out
     terraform apply plan.out
 
-elif [[ "$1" == "destroy" && "$PWD" == "$gcp_directory" ]]; then
+elif [[ "$1" == "destroy" && "$PWD" == "$gcp_directory_path" ]]; then
     terraform destroy -plan=plan.out
 
 else
