621b94db49d08b45ce6a5a583be2cb228a181a2e
Walid Baruni <wdbaruni@gmail.com>
2023-11-06 15:50:09 +0200

    log orchestration: add region output
    Former-commit-id: 938e09e494f15fd0e8348da49cdc4379a462c8ac

--- log-orchestration/cdk/bin/cdk.ts
+++ log-orchestration/cdk/bin/cdk.ts
@@ -7,7 +7,7 @@ const keyName = app.node.tryGetContext('keyName');
 const bacalhauVersion = app.node.tryGetContext('bacalhauVersion') || 'v1.1.3';
 const targetPlatform = app.node.tryGetContext('targetPlatform') || 'linux_amd64';
 const orchestratorInstanceType = app.node.tryGetContext('orchestratorInstanceType') || 't3.micro';
-const webServerInstanceType = app.node.tryGetContext('webServerInstanceType') || 't3.medium';
+const webServerInstanceType = app.node.tryGetContext('webServerInstanceType') || 't3.large';
 const webServerInstanceCount = app.node.tryGetContext('webServerInstanceCount') || 3;
 const openSearchInstanceType = app.node.tryGetContext('openSearchInstanceType') || 't3.small.search';
 
