c4ea0561e5b3851aa43a62f803d19ef671e24512
Ross Jones <ross@register-dynamics.co.uk>
2023-11-15 16:11:47 +0000

    Updated dependencies list (#179)
    * Updated dependencies list
    
    * Update when to run binaries

--- mycelial-demos/mycelial-execution-demo/dependencies.ps1
+++ mycelial-demos/mycelial-execution-demo/dependencies.ps1
@@ -1,23 +1,31 @@
 # Attempts to download all of the necessary tools to build 
 # the demo
 
-function Add-Path($Path) {
-    $Path = [Environment]::GetEnvironmentVariable("PATH", "Machine") + [IO.Path]::PathSeparator + $Path
-    [Environment]::SetEnvironmentVariable( "Path", $Path, "Machine" )
-}
+# Windows build tools
+Invoke-WebRequest -Uri "https://aka.ms/vs/17/release/vc_redist.x64.exe"  -OutFile "vc_redist.x64.exe"
+Invoke-WebRequest -Uri "https://aka.ms/vs/17/release/vs_BuildTools.exe" -OutFile "vs_BuildTools.exe"
+./vc_redist.x64.exe
+./vs_BuildTools.exe 
+
 
 # First up, golang.
-Invoke-WebRequest -Uri "https://go.dev/dl/go1.20.11.windows-amd64.msi" -OutFile "C:\Users\mycelial\go1.20.11.windows-amd64.msi"
+Invoke-WebRequest -Uri "https://go.dev/dl/go1.20.11.windows-amd64.msi" -OutFile "go1.20.11.windows-amd64.msi"
 
+# Nodejs
+Invoke-WebRequest -Uri "https://nodejs.org/dist/v20.9.0/node-v20.9.0-x64.msi" -OutFile "node-v20.9.0-x64.msi"
 
 # Service manager 
-Invoke-WebRequest -Uri "https://nssm.cc/release/nssm-2.24.zip" -OutFile "C:\Users\mycelial\nssm-2.24.zip"
+Invoke-WebRequest -Uri "https://nssm.cc/release/nssm-2.24.zip" -OutFile "nssm-2.24.zip"
 Expand-Archive -LiteralPath 'C:\Users\mycelial\nssm-2.24.zip' -DestinationPath C:\Users\mycelial\
 
 # Rust/Cargo 
-Invoke-WebRequest -Uri "https://static.rust-lang.org/rustup/dist/i686-pc-windows-gnu/rustup-init.exe" -OutFile "C:\Users\mycelial\rustup-init.exe"
-# set PATH=%PATH%;/Users/mycelial/.cargo/bin
+Invoke-WebRequest -Uri "https://static.rust-lang.org/rustup/dist/i686-pc-windows-gnu/rustup-init.exe" -OutFile "rustup-init.exe"
+# set PATH=%PATH%;/Users/USERNAME/.cargo/bin
 
 # Github client for build 
 Invoke-WebRequest -Uri "https://central.github.com/deployments/desktop/desktop/latest/win32" -OutFile "github-install.exe"
-github-install.exe 
+
+start ./go1.20.11.windows-amd64.msi
+start ./node-v20.9.0-x64.msi
+start ./rustup-init.exe
+start ./github-install.exe 
