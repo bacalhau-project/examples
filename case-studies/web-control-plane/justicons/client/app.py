2036d8d88380d7675aa65b2d7fa0632c2c088de7
David Aronchick <aronchick@gmail.com>
2024-03-23 17:07:19 +0100

    updating app.py with more information

--- case-studies/web-control-plane/justicons/client/app.py
+++ case-studies/web-control-plane/justicons/client/app.py
@@ -1,4 +1,5 @@
 # app.py
+import random
 import socket
 from pathlib import Path
 from random import randint
@@ -45,42 +46,54 @@ def index():
     )
 
 
+@app.route("/json-test")
+def json_testing():
+    return json(testing=True)
+
+
 @app.route("/json")
-def json():
-    # Read from color.txt
+def json(testing):
+    if testing:
+        testNode = test_node()
+        ip = testNode["ip"]
+        hostname = testNode["hostname"]
+        zone = testNode["zone"]
+        region = testNode["region"]
+    else:
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(socket.gethostname())
+        node_info = Path("/etc/bacalhau-node-info")
+        zone = "N/A"
+        region = "N/A"
+        if node_info.exists():
+            # Read from /etc/bacalhau-node-info and get ZONE= and REGION=
+            with open(node_info, "r") as file:
+                lines = file.readlines()
+                for line in lines:
+                    if "ZONE=" in line:
+                        zone = line.split("=")[1].replace("\n", "")
+                    if "REGION=" in line:
+                        region = line.split("=")[1].replace("\n", "")
+
     with open("./color.txt", "r") as file:
         colorFromFile = file.read().replace("\n", "")
 
-    hostname = socket.gethostname()
     iconID = f"node-number-{hostname}-icon"
-    hash = hashCode(hostname)
+    hashCodeValue = generateHashCode(hostname)
 
-    node_info = Path("/etc/bacalhau-node-info")
-    zone = "N/A"
-    region = "N/A"
-    if node_info.exists():
-        # Read from /etc/bacalhau-node-info and get ZONE= and REGION=
-        with open(node_info, "r") as file:
-            lines = file.readlines()
-            for line in lines:
-                if "ZONE=" in line:
-                    zone = line.split("=")[1].replace("\n", "")
-                if "REGION=" in line:
-                    region = line.split("=")[1].replace("\n", "")
-
-    return {
-        "icon": Icons.getRand(),
-        "hostname": hostname,
-        "ip": socket.gethostbyname(socket.gethostname()),
-        "color": colorFromFile,
-        "iconID": iconID,
-        "hashCode": hash,
-        "zone": zone,
-        "region": region,
-    }
+    return generate_node(
+        icon=Icons.getRand(),
+        hostname=hostname,
+        ip=ip,
+        color=colorFromFile,
+        iconID=iconID,
+        hashCode=hashCodeValue,
+        zone=zone,
+        region=region,
+    )
 
 
-def hashCode(str):
+def generateHashCode(str):
     hash_value = 0
     for char in str:
         hash_value = (hash_value << 5) - hash_value + ord(char)
@@ -107,7 +120,38 @@ def base36_encode(integer):
     return sign + base36 if sign else base36
 
 
-# return (hash >>> 0).toString(36).padStart(7, '0');
+def test_node():
+    n = random.choice(TEST_NODES_STATICS)
+    return {"ip": n[0], "hostname": n[1], "region": n[2], "zone": n[3]}
+
+
+def generate_node(icon, hostname, ip, color, iconID, hashCode, zone, region):
+    return {
+        "icon": icon,
+        "hostname": hostname,
+        "ip": ip,
+        "color": color,
+        "iconID": iconID,
+        "hashCode": hashCode,
+        "zone": zone,
+        "region": region,
+    }
+
+
+TEST_NODES_STATICS = (
+    ["256.256.256.1", "silva-odonnell.net", "us-west1", "us-west1-a"],
+    ["256.256.256.2", "campbell.org", "us-west1", "us-west1-a"],
+    ["256.256.256.3", "archer-patel.org", "us-west1", "us-west1-b"],
+    ["256.256.256.4", "collins.com", "us-west1", "us-west1-b"],
+    ["256.256.256.5", "rivera.com", "us-east1", "us-east1-a"],
+    ["256.256.256.6", "bowman.info", "us-east1", "us-east1-a"],
+    ["256.256.256.7", "green.com", "us-east1", "us-east1-b"],
+    ["256.256.256.8", "turner.com", "us-east1", "us-east1-b"],
+    ["256.256.256.9", "cortez.com", "eu-west1", "eu-west1-a"],
+    ["256.256.256.10", "snyder.com", "eu-west1", "eu-west1-a"],
+    ["256.256.256.11", "harrell.com", "eu-west1", "eu-west1-b"],
+    ["256.256.256.12", "mclean.net", "eu-west1", "eu-west1-b"],
+)
 
 
 if __name__ == "__main__":
