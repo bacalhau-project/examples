d8d54762f4c9d5d5436dd40443aa4f01195eead7
David Aronchick <aronchick@gmail.com>
2024-03-29 12:08:44 -0700

    justicons working except for clearing old images

--- case-studies/web-control-plane/justicons/terraform/node_files/start_bacalhau.sh
+++ case-studies/web-control-plane/justicons/terraform/node_files/start_bacalhau.sh
@@ -12,7 +12,7 @@ export CONNECT_PEER="none"
 export IP=$(ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -E '^10\.' | head -n 1)
 
 if [[ -n "${IP}" ]]; then
-  export BACALHAU_PREFERRED_ADDRESS="${IP}"
+    export BACALHAU_PREFERRED_ADDRESS="${IP}"
 fi
 
 # export BACALHAU_NODE_CLIENTAPI_HOST=10.128.0.2
@@ -24,33 +24,33 @@ fi
 
 # if the file /etc/bacalhau-bootstrap exists
 if [[ -f /etc/bacalhau-bootstrap ]]; then
-  # shellcheck disable=SC1090
-  source /etc/bacalhau-bootstrap
+    # shellcheck disable=SC1090
+    source /etc/bacalhau-bootstrap
 fi
 
 # If /etc/bacalhau-node-info exists, then load the variables from it
 if [[ -f /etc/bacalhau-node-info ]]; then
-  # shellcheck disable=SC1090
-  . /etc/bacalhau-node-info
+    # shellcheck disable=SC1090
+    . /etc/bacalhau-node-info
 fi
 
 labels="ip=${IP}"
 
 # If REGION is set, then we can assume all labels are set, and we should add it to the labels
 if [[ -n "${REGION}" ]]; then
-  labels="${labels},region=${REGION},zone=${ZONE},appname=${APPNAME}"
+    labels="${labels},region=${REGION},zone=${ZONE},appname=${APPNAME}"
 fi
 
 bacalhau serve \
-    --node-type=compute \
-    --network=${BACALHAU_NODE_NETWORK_TYPE} \
-    --orchestrators=${BACALHAU_NODE_NETWORK_ORCHESTRATORS} \
-    --private-internal-ipfs \
-    --ipfs-swarm-addrs=${BACALHAU_NODE_IPFS_SWARMADDRESSES} \
-    --allow-listed-local-paths=${ABSOLUTE_LOCAL_PATH} \
-    --job-selection-data-locality anywhere \
-    --swarm-port 1235 \
-    --api-port 1234 \
-    --peer "${CONNECT_PEER}" \
-    --job-selection-accept-networked \
-    --labels "${labels}"
+--node-type=compute \
+--network="${BACALHAU_NODE_NETWORK_TYPE}" \
+--orchestrators="${BACALHAU_NODE_NETWORK_ORCHESTRATORS}" \
+--private-internal-ipfs \
+--ipfs-swarm-addrs="${BACALHAU_NODE_IPFS_SWARMADDRESSES}" \
+--allow-listed-local-paths="${ABSOLUTE_LOCAL_PATH}:rw" \
+--job-selection-data-locality anywhere \
+--swarm-port 1235 \
+--api-port 1234 \
+--peer "${CONNECT_PEER}" \
+--job-selection-accept-networked \
+--labels "${labels}"
