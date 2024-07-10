f56b5296b47411ffd18de7769ab5ced1abc0507c
David Aronchick <aronchick@gmail.com>
2024-07-10 19:01:03 +0100

    shares now working

--- queuing/lots_of_jobs.sh
+++ queuing/lots_of_jobs.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 export NUMBER_OF_JOBS=${NUMBER_OF_JOBS:-10000}
+export TIME_BETWEEN_JOBS=${TIME_BETWEEN_JOBS:-30}
+export JOBS_PER_BATCH=${JOBS_PER_BATCH:-20}
 
 for ((i=1; i<=NUMBER_OF_JOBS; i++)); do
     echo -n "$(date +"%Y-%m-%d %H:%M:%S") - "
-    bacalhau job run --id-only --wait=0 stress_job.yaml | tr -d '\n'
+    bacalhau job run --wait=false --id-only stress_job.yaml | tr -d '\n'
     echo ""
     # If mod 5, then wait 30 seconds
-    if [ $((i % 20)) -eq 0 ]; then
-        sleep 5 
+    if [ $((i % JOBS_PER_BATCH)) -eq 0 ]; then
+        sleep "$TIME_BETWEEN_JOBS" 
     fi
 done
 
