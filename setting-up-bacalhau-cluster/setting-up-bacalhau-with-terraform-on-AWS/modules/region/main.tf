7ad8064121b0424d41cab7009ba03657769d5dd5
David Aronchick <aronchick@gmail.com>
2024-12-31 17:44:23 -0800

    chore: Update Terraform configuration and instance module parameters
    Former-commit-id: 237f1234f9f951595de7d2f686f824a1336a320d

--- setting-up-bacalhau-cluster/setting-up-bacalhau-with-terraform-on-AWS/modules/region/main.tf
+++ setting-up-bacalhau-cluster/setting-up-bacalhau-with-terraform-on-AWS/modules/region/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
 
 module "networkModule" {
@@ -23,21 +32,20 @@ module "instanceModule" {
 
   source = "../instance"
 
-  aws_instance_type        = var.aws_instance_type
-  instance_ami             = var.locations[var.region].instance_ami
-  region                   = var.region
-  zone                     = var.locations[var.region].zone
-  vpc_id                   = module.networkModule.vpc_id
-  subnet_public_id         = module.networkModule.public_subnets[0]
-  security_group_ids       = [module.securityGroupModule.sg_22, module.securityGroupModule.sg_4222]
-  app_tag                  = "${var.app_tag}-${count.index}"
-  public_key               = var.public_key
-  private_key              = var.private_key
-  app_name                 = var.app_name
-  bacalhau_installation_id = var.bacalhau_installation_id
-  bacalhau_data_dir        = var.bacalhau_data_dir
-  bacalhau_node_dir        = var.bacalhau_node_dir
-  username                 = var.username
+  aws_instance_type         = var.aws_instance_type
+  instance_ami              = var.locations[var.region].instance_ami
+  region                    = var.region
+  zone                      = var.locations[var.region].zone
+  vpc_id                    = module.networkModule.vpc_id
+  subnet_public_id          = module.networkModule.public_subnets[0]
+  security_group_ids        = [module.securityGroupModule.sg_22, module.securityGroupModule.sg_4222]
+  app_tag                   = "${var.app_tag}-${count.index}"
+  public_key                = var.public_key
+  private_key               = var.private_key
+  bacalhau_data_dir         = var.bacalhau_data_dir
+  bacalhau_node_dir         = var.bacalhau_node_dir
+  bacalhau_config_file_path = var.bacalhau_config_file_path
+  username                  = var.username
 }
 
 output "public_ips" {
