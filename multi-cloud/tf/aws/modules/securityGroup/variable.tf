befdb62366540cb9b9188f1bef1b32d47399438f
David Aronchick <aronchick@gmail.com>
2025-01-02 12:55:22 -0800

    adding gcp

--- multi-cloud/tf/aws/modules/securityGroup/variable.tf
+++
@@ -1,8 +0,0 @@
-variable "vpc_id" {
-  description = "VPC id"
-  type        = string
-}
-variable "app_tag" {
-  description = "Environment tag"
-  type        = string
-}
--- setting-up-bacalhau-cluster/setting-up-bacalhau-with-terraform-on-AWS/modules/region/main.tf
+++ setting-up-bacalhau-cluster/setting-up-bacalhau-with-terraform-on-AWS/modules/region/main.tf
@@ -1,71 +1,55 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "aws" {
-  alias  = "primary"
-  region = var.region
-}
-
-
 module "networkModule" {
-  source  = "../network"
+  source = "../network"
+
   app_tag = var.app_tag
   region  = var.region
-  zone    = var.locations[var.region].zone
+  zone    = var.zone
 
   cidr_block_range         = "10.0.0.0/16"
   subnet1_cidr_block_range = "10.0.1.0/24"
   subnet2_cidr_block_range = "10.0.2.0/24"
-  
+
   providers = {
-    aws = aws.primary
+    aws = aws
   }
 }
 
 module "securityGroupModule" {
   source = "../securityGroup"
 
+  region  = var.region
   vpc_id  = module.networkModule.vpc_id
   app_tag = var.app_tag
-  
+
   providers = {
-    aws = aws.primary
+    aws = aws
   }
 }
 
 module "instanceModule" {
-  count = var.locations[var.region].node_count
-
   source = "../instance"
 
   aws_instance_type         = var.aws_instance_type
-  instance_ami              = var.locations[var.region].instance_ami
+  instance_ami              = var.instance_ami
   region                    = var.region
-  zone                      = var.locations[var.region].zone
+  zone                      = var.zone
   vpc_id                    = module.networkModule.vpc_id
   subnet_public_id          = module.networkModule.public_subnets[0]
   security_group_ids        = [module.securityGroupModule.sg_22, module.securityGroupModule.sg_4222]
-  app_tag                   = "${var.app_tag}-${count.index}"
+  app_tag                   = var.app_tag
   public_key                = var.public_key
   private_key               = var.private_key
   bacalhau_data_dir         = var.bacalhau_data_dir
   bacalhau_node_dir         = var.bacalhau_node_dir
   bacalhau_config_file_path = var.bacalhau_config_file_path
   username                  = var.username
-  
+
   providers = {
-    aws = aws.primary
+    aws = aws
   }
 }
 
 output "public_ips" {
-  value = module.instanceModule[*].public_ip
+  value = module.instanceModule.public_ip
 }
 
