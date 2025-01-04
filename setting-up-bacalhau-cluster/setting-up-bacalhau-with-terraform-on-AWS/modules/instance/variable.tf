0a30b7400ca4777c8d9bee02468cb2d5f1a11815
David Aronchick <aronchick@gmail.com>
2025-01-04 15:50:52 -0800

    feat: add Terraform output and variable files for AWS instance module

---
+++ setting-up-bacalhau-cluster/setting-up-bacalhau-with-terraform-on-AWS/modules/instance/variable.tf
@@ -0,0 +1,69 @@
+# Variables
+
+variable "vpc_id" {
+  description = "VPC id"
+  type        = string
+}
+variable "subnet_public_id" {
+  description = "VPC public subnet id"
+  type        = string
+}
+variable "security_group_ids" {
+  description = "EC2 ssh security group"
+  type        = list(string)
+  default     = []
+}
+variable "app_tag" {
+  description = "Environment tag"
+  type        = string
+}
+
+variable "public_key" {
+  description = "OpenSSH public key"
+  type        = string
+}
+
+variable "private_key" {
+  description = "OpenSSH private key"
+  type        = string
+}
+
+variable "aws_instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "instance_ami" {
+  description = "EC2 instance AMI"
+  type        = string
+}
+
+variable "zone" {
+  description = "AWS zone"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "bacalhau_data_dir" {
+  description = "Bacalhau data directory"
+  type        = string
+}
+
+variable "bacalhau_node_dir" {
+  description = "Bacalhau node directory"
+  type        = string
+}
+
+variable "bacalhau_config_file_path" {
+  description = "Path to the Bacalhau configuration file"
+  type        = string
+}
+
+variable "username" {
+  description = "Username"
+  type        = string
+}
