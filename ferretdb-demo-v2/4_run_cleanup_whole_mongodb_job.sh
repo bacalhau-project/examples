fbda05d82c70df5395f4d5e5ed57073c5512597f
krzychdre <krzysztof.drezewski@gmail.com>
2025-04-15 17:11:49 +0200

    ferretdb sensors demo in docker compose (#233)
    * ferretdb sensors demo in docker compose
    
    * readme added
    
    * - couple of ports hidden from host
    - minor comments on troubleshooting in Readme.md
    
    ---------
    
    Co-authored-by: David Aronchick <aronchick@gmail.com>
    Former-commit-id: 3e02315ffbc1209a335479d02c08cb41090acb26

---
+++ ferretdb-demo-v2/4_run_cleanup_whole_mongodb_job.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "x${FERRETDB_URI}" == "x" ]; then
+  echo "There is no FERRETDB_URI defined (i.e. mongodb://username:password@127.0.0.1/postgres)"
+  exit 1
+fi
+
+bacalhau job run /jobs/clean_sensor_logs_data_job.yaml --template-vars "code=$(cat /scripts/cleanup_mongodb.py | base64 -w0),ferretdb_uri=${FERRETDB_URI}"
