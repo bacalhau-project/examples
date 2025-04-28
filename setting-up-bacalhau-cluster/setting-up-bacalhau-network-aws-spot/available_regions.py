078ea07c251daaf8a8522d6dfce58b5b2f22e864
David Aronchick <aronchick@gmail.com>
2025-04-28 16:47:35 -0700

    updating logs and testing
    Former-commit-id: a530017a33169d3a1d37ceddeb0264166dd8b855

--- setting-up-bacalhau-cluster/setting-up-bacalhau-network-aws-spot/available_regions.py
+++ setting-up-bacalhau-cluster/setting-up-bacalhau-network-aws-spot/available_regions.py
@@ -35,35 +35,35 @@ AVAILABLE_REGIONS = [
 
 # Detailed information about each region's smallest suitable instance
 REGION_DETAILS = {
-    "ap-southeast-4": {
-        "instance_type": "t4g.medium",
-        "vcpus": 2,
-        "memory_gib": 4.0,
-        "spot_price": 0.007600,
-    },
     "ap-south-2": {
         "instance_type": "t4g.medium",
         "vcpus": 2,
         "memory_gib": 4.0,
-        "spot_price": 0.008600,
+        "spot_price": 0.009000,
     },
     "ap-southeast-3": {
         "instance_type": "t4g.medium",
         "vcpus": 2,
         "memory_gib": 4.0,
-        "spot_price": 0.011600,
+        "spot_price": 0.010900,
     },
-    "eu-south-1": {
+    "ap-southeast-4": {
         "instance_type": "t4g.medium",
         "vcpus": 2,
         "memory_gib": 4.0,
-        "spot_price": 0.012100,
+        "spot_price": 0.011400,
     },
     "ap-east-1": {
         "instance_type": "t4g.medium",
         "vcpus": 2,
         "memory_gib": 4.0,
-        "spot_price": 0.012500,
+        "spot_price": 0.012200,
+    },
+    "me-south-1": {
+        "instance_type": "t4g.medium",
+        "vcpus": 2,
+        "memory_gib": 4.0,
+        "spot_price": 0.012600,
     },
     "eu-central-2": {
         "instance_type": "t4g.medium",
@@ -71,13 +71,13 @@ REGION_DETAILS = {
         "memory_gib": 4.0,
         "spot_price": 0.013100,
     },
-    "me-central-1": {
+    "eu-south-1": {
         "instance_type": "t4g.medium",
         "vcpus": 2,
         "memory_gib": 4.0,
         "spot_price": 0.013300,
     },
-    "me-south-1": {
+    "me-central-1": {
         "instance_type": "t4g.medium",
         "vcpus": 2,
         "memory_gib": 4.0,
@@ -87,25 +87,25 @@ REGION_DETAILS = {
         "instance_type": "t4g.medium",
         "vcpus": 2,
         "memory_gib": 4.0,
-        "spot_price": 0.014800,
+        "spot_price": 0.015800,
     },
     "af-south-1": {
-        "instance_type": "t4g.medium",
+        "instance_type": "t3.medium",
         "vcpus": 2,
         "memory_gib": 4.0,
-        "spot_price": 0.016000,
+        "spot_price": 0.016200,
     },
     "ap-southeast-5": {
         "instance_type": "t4g.medium",
         "vcpus": 2,
         "memory_gib": 4.0,
-        "spot_price": 0.018900,
+        "spot_price": 0.018600,
     },
     "ap-northeast-3": {
         "instance_type": "t3.medium",
         "vcpus": 2,
         "memory_gib": 4.0,
-        "spot_price": 0.020300,
+        "spot_price": 0.018700,
     },
     "eu-north-1": {
         "instance_type": "c6i.large",
@@ -114,99 +114,99 @@ REGION_DETAILS = {
         "spot_price": 0.028300,
     },
     "ap-south-1": {
-        "instance_type": "m6idn.large",
+        "instance_type": "m6i.large",
         "vcpus": 2,
         "memory_gib": 8.0,
-        "spot_price": 0.029200,
-    },
-    "us-east-2": {
-        "instance_type": "m6idn.large",
-        "vcpus": 2,
-        "memory_gib": 8.0,
-        "spot_price": 0.029700,
-    },
-    "sa-east-1": {
-        "instance_type": "m5zn.large",
-        "vcpus": 2,
-        "memory_gib": 8.0,
-        "spot_price": 0.030000,
+        "spot_price": 0.032000,
     },
     "ap-northeast-2": {
         "instance_type": "m6i.large",
         "vcpus": 2,
         "memory_gib": 8.0,
-        "spot_price": 0.030600,
+        "spot_price": 0.032800,
     },
     "us-west-1": {
-        "instance_type": "m6idn.large",
-        "vcpus": 2,
-        "memory_gib": 8.0,
-        "spot_price": 0.038100,
-    },
-    "ca-central-1": {
         "instance_type": "m6i.large",
         "vcpus": 2,
         "memory_gib": 8.0,
-        "spot_price": 0.042200,
-    },
-    "us-east-1": {
-        "instance_type": "m6idn.large",
-        "vcpus": 2,
-        "memory_gib": 8.0,
-        "spot_price": 0.046300,
+        "spot_price": 0.036700,
     },
     "eu-west-3": {
         "instance_type": "m6i.large",
         "vcpus": 2,
         "memory_gib": 8.0,
-        "spot_price": 0.047400,
+        "spot_price": 0.040300,
     },
     "eu-west-2": {
         "instance_type": "m6i.large",
         "vcpus": 2,
         "memory_gib": 8.0,
-        "spot_price": 0.048000,
+        "spot_price": 0.043100,
     },
-    "ap-southeast-1": {
-        "instance_type": "m6i.large",
-        "vcpus": 2,
-        "memory_gib": 8.0,
-        "spot_price": 0.048000,
-    },
-    "eu-south-2": {
+    "us-east-2": {
         "instance_type": "m6idn.large",
         "vcpus": 2,
         "memory_gib": 8.0,
-        "spot_price": 0.049700,
+        "spot_price": 0.046400,
+    },
+    "ca-central-1": {
+        "instance_type": "m6i.large",
+        "vcpus": 2,
+        "memory_gib": 8.0,
+        "spot_price": 0.046900,
+    },
+    "ap-southeast-1": {
+        "instance_type": "m6idn.large",
+        "vcpus": 2,
+        "memory_gib": 8.0,
+        "spot_price": 0.048800,
     },
     "us-west-2": {
         "instance_type": "m6idn.large",
         "vcpus": 2,
         "memory_gib": 8.0,
-        "spot_price": 0.055700,
+        "spot_price": 0.050100,
     },
-    "eu-central-1": {
-        "instance_type": "m6gd.xlarge",
-        "vcpus": 4,
-        "memory_gib": 16.0,
-        "spot_price": 0.063700,
+    "eu-south-2": {
+        "instance_type": "m6idn.large",
+        "vcpus": 2,
+        "memory_gib": 8.0,
+        "spot_price": 0.050300,
+    },
+    "us-east-1": {
+        "instance_type": "m6idn.large",
+        "vcpus": 2,
+        "memory_gib": 8.0,
+        "spot_price": 0.052400,
     },
     "ap-southeast-2": {
         "instance_type": "m6idn.large",
         "vcpus": 2,
         "memory_gib": 8.0,
-        "spot_price": 0.071500,
+        "spot_price": 0.057200,
     },
-    "ap-northeast-1": {
-        "instance_type": "m5dn.large",
+    "eu-central-1": {
+        "instance_type": "m6idn.large",
         "vcpus": 2,
         "memory_gib": 8.0,
-        "spot_price": 0.073800,
+        "spot_price": 0.057500,
+    },
+    "sa-east-1": {
+        "instance_type": "m6i.large",
+        "vcpus": 2,
+        "memory_gib": 8.0,
+        "spot_price": 0.062900,
     },
     "eu-west-1": {
         "instance_type": "m6idn.large",
         "vcpus": 2,
         "memory_gib": 8.0,
-        "spot_price": 0.080700,
+        "spot_price": 0.080200,
+    },
+    "ap-northeast-1": {
+        "instance_type": "m6gd.xlarge",
+        "vcpus": 4,
+        "memory_gib": 16.0,
+        "spot_price": 0.084500,
     },
 }
