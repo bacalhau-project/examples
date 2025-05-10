9522f87b358d0cbfeeec2f63147f56c903a123e1
LukaszSkrzatOC <ext-lukasz.skrzat@opuscapita.com>
2025-05-10 19:34:56 +0200

    S3

--- EnduroSat-demo/scripts/disable-network.sh
+++ EnduroSat-demo/scripts/disable-network.sh
@@ -18,5 +18,11 @@ iptables -I OUTPUT -p udp --dport 4222 -j DROP
 iptables -I FORWARD -p tcp --dport 4222 -j DROP
 iptables -I FORWARD -p udp --dport 4222 -j DROP
 
+iptables -D INPUT -p tcp --dport 9000 -j DROP
+iptables -D OUTPUT -p tcp --dport 9000 -j DROP
+iptables -D FORWARD -p tcp --dport 9000 -j DROP
 
+iptables -D INPUT -p udp --dport 9000 -j DROP
+iptables -D OUTPUT -p udp --dport 9000 -j DROP
+iptables -D FORWARD -p udp --dport 9000 -j DROP
 echo "Block all"
--- EnduroSat-demo/scripts/enable-network.sh
+++ EnduroSat-demo/scripts/enable-network.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
-
-
 echo "Iptables config"
-
 
 delete_rules() {
   local chain=$1
   local proto=$2
+  local port=$3
 
-  while iptables -C "$chain" -p "$proto" --dport 4222 -j DROP 2>/dev/null; do
-    iptables -D "$chain" -p "$proto" --dport 4222 -j DROP
-    echo "Removed: $chain $proto 4222"
+  while iptables -C "$chain" -p "$proto" --dport "$port" -j DROP 2>/dev/null; do
+    iptables -D "$chain" -p "$proto" --dport "$port" -j DROP
+    echo "Removed: $chain $proto $port"
   done
 }
 
 for chain in INPUT OUTPUT FORWARD; do
   for proto in tcp udp; do
-    delete_rules "$chain" "$proto"
+    for port in 4222 9000; do
+      delete_rules "$chain" "$proto" "$port"
+    done
   done
 done
-
 
 echo "Allow All Connect"
