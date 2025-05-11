a072f4d982e83f85b1393822820772ce42d3cf63
David Aronchick <aronchick@gmail.com>
2025-05-10 17:19:48 -0700

    fixing ferret installer

--- ferretdb-demo-v2/4_run_cleanup_whole_mongodb_job.sh
+++ ferretdb-demo-v2/4_run_cleanup_whole_mongodb_job.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ "x${FERRETDB_URI}" == "x" ]; then
+if [ "${FERRETDB_URI}" == "x" ]; then
   echo "There is no FERRETDB_URI defined (i.e. mongodb://expansouser:safepassword@127.0.0.1/postgres)"
-  exit 1
+  exit 1    
 fi
 
-bacalhau job run /jobs/clean_sensor_logs_data_job.yaml --template-vars "code=$(cat /scripts/cleanup_mongodb.py | base64 -w0),ferretdb_uri=${FERRETDB_URI}"
+bacalhau job run /jobs/clean_sensor_logs_data_job.yaml \
+  -V code="$(base64 -w0 < /scripts/cleanup_mongodb.py)" \
+  -V ferretdb_uri="${FERRETDB_URI}"
