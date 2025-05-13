59f4a8d41245017e7edbb073fec0b329ac505a73
krzychdre <krzysztof.drezewski@gmail.com>
2025-05-13 19:01:14 +0200

    - fixed FerretDB access for jobs (as they cannot resolve container name bc they're spawned outside of docker-compose network) (#240)
    - removed 4th job script (cleanup sensor data) as the job was removed previously
    - amended Readme.md - do not mention removed job
    
    Co-authored-by: David Aronchick <aronchick@gmail.com>

--- ferretdb-demo-v2/2_run_sync_sensor_logs_job.sh
+++ ferretdb-demo-v2/2_run_sync_sensor_logs_job.sh
@@ -4,14 +4,12 @@
 # synchronize sqlite from all sensors to FerretDB
 #
 
-if [ "${FERRETDB_URI}" == "x" ]; then
-  echo "There is no FERRETDB_URI defined (i.e. mongodb://expansouser:safepassword@127.0.0.1/postgres)"
-  exit 1
-fi
+# override FERRETDB_URI for job, as it has no access to docker-compose's network and cannot resolve name of FerretDB container
+export FERRET_IP=$(getent hosts ferretdb | awk '{print $1}')
+export FERRETDB_URI="mongodb://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${FERRET_IP}:27017/postgres"
 
 # run sensor logs syncer within daemon job
 bacalhau job run /jobs/run_sensor_logs_syncer.yaml \
   -V code="$(base64 -w0 < /scripts/sqlite_syncer.py)" \
   -V loop_code="$(base64 -w0 < /scripts/sync_loop_script.sh)" \
   -V ferretdb_uri="${FERRETDB_URI}"
-
