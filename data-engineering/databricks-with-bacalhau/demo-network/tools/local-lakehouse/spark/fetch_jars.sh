b55ff29f388d974f82565da6dc4419e82983f7b4
David Aronchick <aronchick@gmail.com>
2025-05-15 14:59:19 -0700

    adding back in sanitization

--- data-engineering/databricks-with-bacalhau/demo-network/tools/local-lakehouse/spark/fetch_jars.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-JAR_DIR="$(dirname "$0")/jars"
-mkdir -p "$JAR_DIR"
-cd "$JAR_DIR"
-
-curl -LO https://repo1.maven.org/maven2/io/delta/delta-spark_2.12/3.0.0/delta-spark_2.12-3.0.0.jar
-curl -LO https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.3.6/hadoop-aws-3.3.6.jar
-curl -LO https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.12.592/aws-java-sdk-bundle-1.12.592.jar
