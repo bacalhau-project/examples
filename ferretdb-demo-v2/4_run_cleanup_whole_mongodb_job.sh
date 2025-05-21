5d59064e7a2a56529872571ceddb404f2df9a979
Krzysztof Dreżewski <krzysztof.drezewski@gmail.com>
2025-05-21 17:33:27 +0200

    pulled latest changes, undeleted Readme(!), fixed 4_run_cleanup_whole_mongodb_job.sh (it was restored lately after removal(?))

--- ferretdb-demo-v2/4_run_cleanup_whole_mongodb_job.sh
+++ ferretdb-demo-v2/4_run_cleanup_whole_mongodb_job.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if [ "${FERRETDB_URI}" == "x" ]; then
-  echo "There is no FERRETDB_URI defined (i.e. mongodb://expansouser:safepassword@127.0.0.1/postgres)"
-  exit 1    
-fi
+# override FERRETDB_URI for job, as it has no access to docker-compose's network and cannot resolve name of FerretDB container
+export FERRET_IP=$(getent hosts ferretdb | awk '{print $1}')
+export FERRETDB_URI="mongodb://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${FERRET_IP}:27017/postgres"
 
 bacalhau job run /jobs/clean_sensor_logs_data_job.yaml \
   -V code="$(base64 -w0 < /scripts/cleanup_mongodb.py)" \
